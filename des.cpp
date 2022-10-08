29364afcef8dabdcf823adfc6c92383616bdbd3d
Controlgame03 <60976264+Controlgame03@users.noreply.github.com>
2022-10-08 17:36:50 +0300

    add function F v1

--- des.cpp
+++ des.cpp
@@ -39,6 +39,16 @@ int IP[IP_ROWS][IP_COLUMNS] =
                  {61, 53, 45, 37, 29, 21, 13, 5},
                  {63, 55, 47, 39, 31, 23, 15, 7}};
 
+int P[P_ROWS][P_COLUMNS] = 
+                {{16, 7, 20, 21},
+                 {29, 12, 28, 17},
+                 {1, 15, 23, 26},
+                 {5, 18, 31, 10},
+                 {2, 8, 24, 14},
+                 {32, 27, 3, 9},
+                 {19, 13, 30, 6},
+                 {22, 11, 4, 25}};
+
 int IP_REVERSE[IP_ROWS][IP_COLUMNS] =
                 {{40, 8, 48, 16, 56, 24, 64, 32},
                  {39, 7, 47, 15, 55, 23, 63, 31},
@@ -48,8 +58,67 @@ int IP_REVERSE[IP_ROWS][IP_COLUMNS] =
                  {35, 3, 43, 11, 51, 19, 59, 27},
                  {34, 2, 42, 10, 50, 18, 58, 26},
                  {33, 1, 41, 9, 49, 17, 57, 25}};
-                
 
+int E_BIT_SELECTION_TABLE[E_ROWS][E_COLUMNS] =
+                {{32, 1, 2, 3, 4, 5},
+                 {4, 5, 6, 7, 8, 9},
+                 {8, 9, 10, 11, 12, 13},
+                 {12, 13, 14, 15, 16, 17},
+                 {16, 17, 18, 19, 20, 21},
+                 {20, 21, 22, 23, 24, 25},
+                 {24, 25, 26, 27, 28, 29},
+                 {28, 29, 30, 31, 32, 1}};
+                
+int S_BOX[S_BOX_NUMBER][S_BOX_ROWS][S_BOX_COLUMNS] = {
+		{
+			{ 14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7 },
+			{ 0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8 },
+			{ 4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0 },
+			{ 15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13 } 
+		},
+		{
+			{ 15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10 },
+			{ 3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5 },
+			{ 0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15 },
+			{ 13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9 }
+		},
+		{
+			{ 10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8 },
+			{ 13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1 },
+			{ 13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7 },
+			{ 1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12 }
+		},
+		{
+			{ 7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15 },
+			{ 13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9 },
+			{ 10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4 },
+			{ 3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14 }
+		},
+		{
+			{ 2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9 },
+			{ 14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6 },
+			{ 4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14 },
+			{ 11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3 }
+		},
+		{
+			{ 12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11 },
+			{ 10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8 },
+			{ 9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6 },
+			{ 4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13 }
+		},
+		{
+			{ 4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1 },
+			{ 13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6 },
+			{ 1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2 },
+			{ 6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12 }
+		},
+		{
+			{ 13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7 },
+			{ 1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2 },
+			{ 7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8 },
+			{ 2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11 }
+		}
+	};
 
 int LEFT_SHIFTS_NUMBER[ROUNDS] = {1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1};
 
@@ -57,26 +126,17 @@ Des::Des(bitset<EXTENDED_KEY_SIZE>& exKey){
     extendedKey = exKey;
 }
 
-void Des::encryption(){
-    //1. Написать блок для разбивки ключа на 16 подключей (complete)
+bitset<MESSAGE_SIZE> Des::encryption(){
     createKeys();
-    //2. Написать блок работы функции f (главной функции)
-    //3. Написать блок работы сети Файстеля (complete)
+    return feistelСipher();
 }
 
 void Des::test(){
-    this->createKeys();
-
-    cout << "subkeys for testKey:\n\n";
-    for(int i = 0; i < ROUNDS; i++){
-        cout << "subkey " << i + 1 << " ---> " << subkey[i].to_string() << "\n" << endl;
-    }
-
     string messageStr("0000000100100011010001010110011110001001101010111100110111101111");
     reverse(messageStr.begin(), messageStr.end());
     bitset<MESSAGE_SIZE> messageTmp(messageStr);
     this->message = messageTmp;
-    this->feistelСipher();
+    cout << "encryption: " << this->encryption() << endl;
 }
 
 void Des::decryption(){
@@ -164,33 +224,20 @@ bitset<MESSAGE_SIZE> Des::feistelСipher(){
     while(messageIterator < MESSAGE_SIZE/2) leftMessageHalf[halfIterator++] = permutationMessage[messageIterator++];
     halfIterator = 0;
     while(messageIterator < MESSAGE_SIZE) rightMessageHalf[halfIterator++] = permutationMessage[messageIterator++];
-    cout << leftMessageHalf << "\n" << rightMessageHalf << endl;
     
     //Faistel cipher
     bitset<MESSAGE_SIZE/2> nextLeftHalf = leftMessageHalf;
     bitset<MESSAGE_SIZE/2> nextRightHalf = rightMessageHalf;
-    for(int roundCounter; roundCounter < ROUNDS; roundCounter++){
+    for(int roundCounter = 0; roundCounter < ROUNDS; roundCounter++){
         leftMessageHalf = nextLeftHalf;
         rightMessageHalf = nextRightHalf;
 
         nextLeftHalf = rightMessageHalf;
         nextRightHalf = leftMessageHalf ^ functionF(rightMessageHalf, subkey[roundCounter]);
+        /*error*/
+        cout << "right half: " << nextRightHalf << endl;
     }
-/*
-    //test
-    string l1("01000011010000100011001000110100");
-    reverse(l1.begin(), l1.end());
-    bitset<MESSAGE_SIZE/2> lh(l1);
-    
 
-    string r1("00001010010011001101100110010101");
-    reverse(r1.begin(), r1.end());
-    bitset<MESSAGE_SIZE/2> rh(r1);
-
-    nextLeftHalf = lh;
-    nextRightHalf = rh;
-    //test end
-*/
     //concatenated two halfs = rightHalf + leftHalf
     int permutationCounter = 0;
     for(int rightCounter = 0; rightCounter < MESSAGE_SIZE/2; rightCounter++)
@@ -210,7 +257,68 @@ bitset<MESSAGE_SIZE> Des::feistelСipher(){
 }
 
 bitset<MESSAGE_SIZE/2> Des::functionF(bitset<MESSAGE_SIZE/2> rightHalf, bitset<SUBKEY_SIZE> key){
-    return rightHalf;
+    bitset<SUBKEY_SIZE> extendedHalf;
+    bitset<MESSAGE_SIZE/2> result;
+    bitset<MESSAGE_SIZE/2> permutationResult;
+
+    int resultIterator = 0;
+
+    int messageIterator = 0;
+    for(int row = 0; row < E_ROWS; row++){
+        for(int column = 0; column < E_COLUMNS; column++){
+            extendedHalf[messageIterator++] = rightHalf[E_BIT_SELECTION_TABLE[row][column] - 1];
+        }
+    }
+
+    extendedHalf = extendedHalf ^ key;
+
+    int halfCounter = 0;
+    int blockCounter = 0;
+    for(int round = 0; round < F_FUNCTION_ROUNDS; round++){
+        bitset<F_FUNCTION_BLOCKS> currentBlock;
+
+        for(blockCounter = 0; blockCounter < F_FUNCTION_BLOCKS; blockCounter++) 
+            currentBlock[blockCounter] = extendedHalf[halfCounter++];
+        
+        bitset<S_BOX_ROW_POSITION> reverseSBoxRow;
+        reverseSBoxRow[0] = currentBlock[0];
+        reverseSBoxRow[1] = currentBlock[F_FUNCTION_BLOCKS - 1];
+
+        bitset<S_BOX_COLUMN_POSITION> reverseSBoxColumn;
+        for(int iterator = 0; iterator < S_BOX_COLUMN_POSITION; iterator++)
+            reverseSBoxColumn[iterator] = currentBlock[iterator + 1];
+
+        string tmpForReverseRow = reverseSBoxRow.to_string();
+        reverse(tmpForReverseRow.begin(), tmpForReverseRow.end());
+    
+        string tmpForReverseColumn = reverseSBoxColumn.to_string();
+        reverse(tmpForReverseColumn.begin(), tmpForReverseColumn.end());
+        
+        bitset<S_BOX_ROW_POSITION> sBoxRow(tmpForReverseRow);
+        bitset<S_BOX_COLUMN_POSITION> sBoxColumn(tmpForReverseColumn);
+
+        //cout << "row: " << sBoxRow.to_ulong() << '\n' << endl;
+        //cout << "column: " << sBoxColumn.to_ulong() << '\n' << endl;
+        bitset<S_BOX_RESULT_SIZE> reverseResultBlock(S_BOX[round][sBoxRow.to_ulong()][sBoxColumn.to_ulong()]);
+        
+        string tmpForReverse = reverseResultBlock.to_string();
+        reverse(tmpForReverse.begin(), tmpForReverse.end());
+
+        bitset<S_BOX_RESULT_SIZE> resultBlock(tmpForReverse);
+        for(int blockCounter = 0; blockCounter < S_BOX_RESULT_SIZE; blockCounter++){
+            result[resultIterator++] = resultBlock[blockCounter];
+        }
+    }
+    cout << "result: " << result << "\n" << endl;
+    //final permutation
+    resultIterator = 0;
+    for(int row = 0; row < P_ROWS; row++){
+        for(int column = 0; column < P_COLUMNS; column++){
+            permutationResult[resultIterator++] = result[P[row][column] - 1];
+        }
+    }
+    cout << "permutation result: " << permutationResult << endl;
+    return result;
 }
 
 
--- des.h
+++ des.h
@@ -21,6 +21,25 @@ from https://page.math.tu-berlin.de/~kant/teaching/hess/krypto-ws2006/des.htm
 #define IP_ROWS 8
 #define IP_COLUMNS 8
 
+#define E_ROWS 8
+#define E_COLUMNS 6
+
+#define S_BOX_NUMBER 8
+#define S_BOX_ROWS 4
+#define S_BOX_COLUMNS 16
+
+#define F_FUNCTION_ROUNDS 8
+#define F_FUNCTION_BLOCKS 6 //example: 100100 
+
+//F_FUNCTION_BLOCKS = S_BOX_ROW_POSITION + S_BOX_COLUMN_POSITION
+
+#define S_BOX_ROW_POSITION 2 //[1]0010[O] ---> 10
+#define S_BOX_COLUMN_POSITION 4 //1[0][0][1][0]0 ---> 0010
+#define S_BOX_RESULT_SIZE 4
+
+#define P_ROWS 8
+#define P_COLUMNS 4
+
 using namespace std;
 
 class Des{
@@ -53,7 +72,7 @@ public:
     Des(bitset<EXTENDED_KEY_SIZE>&);
     void test();
 
-    void encryption();
+    bitset<MESSAGE_SIZE> encryption();
     void decryption();
     
 };
